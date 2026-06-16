5b8ac106cc5ed835abd88a8fe5aaf794cd47cf2b
dhaneshwaran <dhaneshwarang13@gmail.com>
2026-06-16 18:48:23 +0530

    created users entity

---
+++ usersecurity/src/main/java/com/example/usersecurity/enitity/Users.java
@@ -0,0 +1,22 @@
+package com.example.usersecurity.enitity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.yaml.snakeyaml.tokens.ScalarToken;
+
+@Data
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long userId;
+
+    private String username;
+    private String password;
+    private String role;
+
+}
